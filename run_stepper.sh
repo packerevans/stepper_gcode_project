71dfedbce26f3c1379653bc6da0407b1b755f0ed
Packer Evans <packerevans@example.com>
2026-04-15 14:23:17 -0600

    Update run_stepper.sh to remove restart loop

--- run_stepper.sh
+++ run_stepper.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
+pkill -f app.py
 cd /home/packer/Videos/stepper_gcode_project/
 source venv/bin/activate
-pkill -f app.py
-while true; do
-    python3 app.py
-    echo "App crashed or was told to exit. Restarting in 1 second..."
-    sleep 1
-done
+
+python3 app.py
