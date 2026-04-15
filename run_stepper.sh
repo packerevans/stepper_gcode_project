c012abda2fdcd0373ed352a5e80895764812f233
packerevans <41245771+packerevans@users.noreply.github.com>
2026-04-15 13:26:09 -0600

    Update run_stepper.sh

--- run_stepper.sh
+++ run_stepper.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-
+pkill -f app.py
 cd stepper_gcode_project/
 source venv/bin/activate
 
-while true; do
-    python3 app.py
-    echo "App crashed or was told to exit. Restarting in 1 second..."
-    sleep 1
-done
+python3 app.py
